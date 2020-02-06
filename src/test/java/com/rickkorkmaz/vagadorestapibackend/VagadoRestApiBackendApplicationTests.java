c3b90286799a28e91d336865261d4d9208b0e6ac
Rick Korkmaz <k.korkmaz@student.han.nl>
2020-02-06 14:22:07 +0100

    Spring MVC Bootstrapped

---
+++ src/test/java/com/rickkorkmaz/vagadorestapibackend/VagadoRestApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickkorkmaz.vagadorestapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagadoRestApiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
